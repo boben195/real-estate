a8837c9961092a02916872bf7055270b9aa3fee3
boben195 <boben195@ukr.net>
2025-03-04 17:41:27 +0200

    add tailwind libs and data

---
+++ react/src/data.jsx
@@ -0,0 +1,237 @@
+import glass1 from "./assets/glass1.jpg";
+import glass2 from "./assets/hero.jpg";
+import glass3 from "./assets/glass3.jpg";
+import smart3 from "./assets/smart3.jpg";
+import smart2 from "./assets/smart2.jpg";
+import smart1 from "./assets/smart1.jpg";
+import apartment1 from "./assets/apartment1.jpg";
+import apartment2 from "./assets/apartment2.jpg";
+import apartment3 from "./assets/apartment3.jpg";
+import bungalow1 from "./assets/bungalow1.jpg";
+import bungalow2 from "./assets/bungalow2.jpg";
+import bungalow3 from "./assets/bungalow3.jpg";
+import office1 from "./assets/office1.jpg";
+import office2 from "./assets/office2.jpg";
+import office3 from "./assets/office3.jpg";
+import villa1 from "./assets/villa1.jpg";
+import villa2 from "./assets/villa2.jpg";
+import villa3 from "./assets/villa3.jpg";
+
+export const properties = [
+  {
+    id: 1,
+    name: "Ocean Breeze Apartment",
+    price: "160000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: smart1,
+    property: "Apartment",
+  },
+  {
+    id: 2,
+    name: "Jackson House ",
+    price: "350000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: apartment1,
+    property: "House",
+  },
+  {
+    id: 3,
+    name: "Lakeside Villa",
+    price: "170000",
+    country: "USA",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: villa1,
+    property: "Villa",
+  },
+  {
+    id: 4,
+    name: "Ocean Breeze Villa",
+    price: "380000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: bungalow1,
+    property: "Villa",
+  },
+  {
+    id: 5,
+    name: "Jackson Office Apartment",
+    price: "280000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: office1,
+    property: "Apartment",
+  },
+  {
+    id: 6,
+    name: "Lakeside House",
+    price: "170000",
+    country: "USA",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: glass1,
+    property: "Apartment",
+  },
+  {
+    id: 7,
+    name: "Ocean Breeze House",
+    price: "350000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: smart2,
+    property: "House",
+  },
+  {
+    id: 8,
+    name: "Jackson Apartment",
+    price: "190000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: apartment2,
+    property: "Apartment",
+  },
+  {
+    id: 9,
+    name: "Lakeside Villa",
+    price: "300000",
+    country: "USA",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: villa2,
+    property: "Villa",
+  },
+  {
+    id: 10,
+    name: "Ocean Breeze Apartment",
+    price: "350000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: bungalow2,
+    property: "Apartment",
+  },
+  {
+    id: 11,
+    name: "Jackson Villa",
+    price: "205000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: office2,
+    property: "Villa",
+  },
+  {
+    id: 12,
+    name: "Lakeside House",
+    price: "300000",
+    country: "UK",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: glass2,
+    property: "House",
+  },
+  {
+    id: 13,
+    name: "Ocean Breeze Apartment",
+    price: "200000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: smart3,
+    property: "Apartment",
+  },
+  {
+    id: 14,
+    name: "Jackson Apartment",
+    price: "205000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: apartment3,
+    property: "Apartment",
+  },
+  {
+    id: 15,
+    name: "Lakeside Villa",
+    price: "300000",
+    country: "USA",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: villa3,
+    property: "Villa",
+  },
+  {
+    id: 16,
+    name: "Ocean Breeze Villa",
+    price: "350000",
+    country: "UK",
+    beds: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 3,
+    image: bungalow3,
+    property: "Villa",
+  },
+  {
+    id: 17,
+    name: "Jackson glass",
+    price: "105000",
+    country: "Canada",
+    beds: 5,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 4,
+    image: office3,
+    property: "House",
+  },
+  {
+    id: 18,
+    name: "Lakeside Cottage",
+    price: "300000",
+    country: "USA",
+    beds: 3,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.",
+    baths: 2,
+    image: glass3,
+    property: "House",
+  },
+];
