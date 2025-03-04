304608c7002afecde754181c974958b3317c5722
boben195 <boben195@ukr.net>
2025-03-04 21:23:56 +0200

    add first and second section

--- react/src/components/Hero.jsx
+++ react/src/components/Hero.jsx
@@ -4,12 +4,16 @@ import hero_img from "../assets/hero2.png";
 const Hero = () => {
   return (
     <div>
-      <div>
-        <div>
-          <h1>
+      <div className="grid h-screen w-full place-items-center bg-cyan-300 rounded-br-[30%] rounded-bl-[30%]">
+        <div className="text-center">
+          <h1 className="text-6xl font-semibold mt-3">
             Find your Dream Home <br /> with Ease
           </h1>
-          <img src={hero_img} alt="Home of your dream" />
+          <img
+            src={hero_img}
+            alt="Home of your dream"
+            className="w-[1400px] h-[750px] object-contain -mt-28"
+          />
         </div>
       </div>
     </div>
--- react/src/components/Search.jsx
+++ react/src/components/Search.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { FaDollarSign, FaMapMArkerAlt } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+      <div>
+        <FaMapMArkerAlt />
+        <div>
+          <p>Location (any)</p>
+          <select>
+            <option value="default">Select your place</option>
+            <option value="USA">USA</option>
+            <option value="CANADA">CANADA</option>
+            <option value="UK">UK</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <FaHome />
+        <div>
+          <p>Property type (any)</p>
+          <select>
+            <option value="default">Select your place</option>
+            <option value="Apartment">Apartment</option>
+            <option value="Villa">Villa</option>
+            <option value="House">House</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <FaDollarSign />
+        <div>
+          <p>Price range (any)</p>
+          <select>
+            <option value="default">Select price range</option>
+            <option value="0-200000">0-200000</option>
+            <option value="200001-300000">200001-300000</option>
+            <option value="300001-400000">300001-400000</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
